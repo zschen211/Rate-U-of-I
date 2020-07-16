8b0960b4f72df352b629d27bf3e75550b9fabd63
tony0 <shichen5@illinois.edu>
2020-07-16 02:26:55 -0500

    setup database

---
+++ apps/import_csv.py
@@ -0,0 +1,16 @@
+import csv
+from apps.models import Place
+
+
+contSuccess = 0
+# Remove all data from Table
+# Place.objects.all().delete()
+
+with open('apps/Restaurant.csv') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    print('Loading...')
+    for row in spamreader:
+        if not Place.objects.filter(placeName=row[0], placeID=row[1]).exists():
+            Place.objects.create(placeName=row[0], placeID=row[1], business_status=row[2], types=row[3], vicinity=row[4], price_level=row[5], rating=float(row[6]), users_rating_num=row[7])
+            contSuccess += 1
+    print(f'{str(contSuccess)} inserted successfully! ')
---
+++ apps/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class Place(models.Model):
+    placeName = models.CharField(max_length=200)
+    placeID = models.CharField(max_length=50, primary_key=True)
+    business_status = models.CharField(max_length=50)
+    types = models.CharField(max_length=100)
+    vicinity = models.CharField(max_length=100)
+    price_level = models.IntegerField(default=0)
+    rating = models.FloatField(default=0)
+    users_rating_num = models.IntegerField(default=0)
+
+# class MyCsvModel(CsvModel):
+#     placeName = models.CharField(max_length=50)
+#     placeID = models.CharField(max_length=50, primary_key=True)
+#     business_status = models.CharField(max_length=50)
+#     types = models.CharField(max_length=100)
+#     vicinity = models.CharField(max_length=100)
+#     price_level = models.IntegerField(default=0)
+#     rating = models.IntegerField(default=0)
+#     users_rating_num = models.IntegerField(default=0)
+
+#     class Meta:
+#         dbModel = Place
+#         delimiter = ";"
+
+class User(models.Model):
+    userID = models.AutoField(default=0, primary_key=True)
+    username = models.IntegerField(default=0)
+    password = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10)
+    country = models.CharField(max_length=20)
+    ethnicity = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+
+class Rating(models.Model):
+    userID = models.ForeignKey('User', on_delete=models.CASCADE,related_name='+')
+    placeID = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='+')
+    user_rating = models.FloatField(default=0)
+
+
+
