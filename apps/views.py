3c7dd24726a42fff17a69fc02fcc34156d471298
tony0 <shichen5@illinois.edu>
2020-07-18 14:31:17 -0500

    ...

--- apps/views.py
+++ apps/views.py
@@ -7,7 +7,7 @@ def search(request):
     if content:
         content = '%' + content + '%'
         place_list = Place.objects.raw(
-            "SELECT placeName, placeID FROM apps_place WHERE placeName LIKE %s OR types LIKE %s ORDER BY rating", [content, content]
+            "SELECT * FROM apps_place WHERE placeName LIKE %s OR types LIKE %s ORDER BY rating", [content, content]
         )
         # place_list = Place.objects.filter(placeName__icontains=content)
         return render(request, 'results.html', {'place_list':place_list})
